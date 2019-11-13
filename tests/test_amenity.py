aca06971a71464ec98c143187faf5d27df759d04
apatinoc4 <apatinoc4@gmail.com>
2019-11-13 16:48:40 -0500

    started Amenity tests and init.py

---
+++ tests/test_amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Test Cases for Amenity model
+"""
+
+import unittest
+from datetime import datetime
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+
+class Amenity_Testing(unittest.TestCase):
+    """Amenity Testing"""
+    
