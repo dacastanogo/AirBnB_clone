07207ce6d7cdd1cbdcf5d2f6290bb02a2dfb8f59
dacastanogo <dcastanogomez@gmail.com>
2019-11-13 13:23:45 -0500

    Added AUTHORS and LICENSE

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 
 from datetime import datetime
-from models.__init__ import storage
 import models
 import uuid
 
@@ -25,8 +24,8 @@ class BaseModel:
                     setattr(self, k, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
             self.save()
 
@@ -43,7 +42,7 @@ class BaseModel:
         Updates the public instance
         attribute updated_at with the current datetime
         """
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
